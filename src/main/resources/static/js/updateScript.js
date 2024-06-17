fbc1aab14259009315205a6e18ce7efff45ba0d0
chebyrach <chebyrach0@gmail.com>
2024-06-17 19:26:00 +0300

    zxc 1000-7

---
+++ src/main/resources/static/js/updateScript.js
@@ -0,0 +1,17 @@
+document.getElementById('updateForm').addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	const id = document.getElementById('id').value;
+	const name = document.getElementById('name').value;
+	const password = document.getElementById('password').value;
+	const gmail = document.getElementById('gmail').value;
+
+	fetch('/updateUser/' + id, {
+		 method: 'POST',
+		 headers: {
+			'Content-Type': 'application/json'
+		},
+		body: `name=${encodeURIComponent(name)}&password=${encodeURIComponent(password)}&gmail=${encodeURIComponent(gmail)}`
+	})
+});
+//localhost:8080/updateUser/24?name=Logan&password=newPass&gmail=wolverine24@gmail.com
